8d8b5a67a98acc58373f0c96ea5287ce160cfdb1
mrloop <ewan@mrloop.com>
2025-05-17 07:24:41 +0100

    Add settings to control URL where preview iframe loaded
    Can't load the iframe from the plugin folder, restrictions on file
    protocol and `<script type='module'/>`. Instead will load from a public
    URL or during development from a local developement server.

--- packages/plugin/main.ts
+++ packages/plugin/main.ts
@@ -1,35 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { loadPrism } from 'obsidian';
+import { loadPrism, Plugin } from 'obsidian';
+import { EmberPluginSettingTab } from 'settings';
+
+
+interface EmberPluginSettings {
+  renderUrl: string;
+}
+
+const DEFAULT_SETTINGS: Partial<EmberPluginSettings> = {
+  renderUrl: 'http://localhost:4200',
+}
 
 export default class EmberPlugin extends Plugin {
+  settings: EmberPluginSettings;
 
 	async onload() {
     this.registerExtensions(['gts', 'gjs'], "javascript");
-    // due to ember using <script type="module"/> can't server file from file system
-    // e.g. let src = this.app.vault.adapter.getResourcePath('dist/index.html');
-    let url = new URL('http://localhost:4200');
-      //or load from service worker
+
+    await this.loadSettings();
+    this.addSettingTab(new EmberPluginSettingTab(this.app, this));
+
+    this.registerEmberProcessor();
+  }
+
+  async registerEmberProcessor() {
+    let url = new URL(this.settings.renderUrl);
+
     ["gjs","gts"].forEach(lang => {
-      this.registerMarkdownCodeBlockProcessor(lang, async (source, el, ctx) => {
+      this.registerMarkdownCodeBlockProcessor(lang, async (source, el, _ctx) => {
         const highlightElement = async (el: HTMLElement) => {
           const prism = await loadPrism();
           prism.highlightElement(el);
         };
-
         let pre = el.createEl('pre');
         let code = pre.createEl('code', {cls: 'language-js', text: source});
         highlightElement(code)
 
         url.searchParams.set('source', source);
         url.searchParams.set('lang', lang);
-        //return "<iframe src='https://mrloop.com'></iframe>";
         const iframe = el.createEl('iframe');
         iframe.src = url.toString();
         iframe.width = '100%';
         el.appendChild(iframe);
       });
     })
+
   }
 
-	onunload() {}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
