ec7d3384591ce0c47bedf469863d0e221b1eb9a0
mrloop <ewan@mrloop.com>
2025-05-17 08:31:41 +0100

    Conditionally use local or remote server depending build env

--- packages/plugin/main.ts
+++ packages/plugin/main.ts
@@ -1,20 +1,25 @@
-import { loadPrism, Plugin } from 'obsidian';
-import { EmberPluginSettingTab } from 'settings';
+import { loadPrism, Plugin } from "obsidian";
+import { EmberPluginSettingTab } from "settings";
 
+declare global {
+  const __IS_PRODUCTION__: boolean;
+}
 
 interface EmberPluginSettings {
   renderUrl: string;
 }
 
 const DEFAULT_SETTINGS: Partial<EmberPluginSettings> = {
-  renderUrl: 'http://localhost:4200',
-}
+  renderUrl: __IS_PRODUCTION__
+    ? "https://obsidian-ember.netlify.app"
+    : "http://localhost:4200",
+};
 
 export default class EmberPlugin extends Plugin {
   settings: EmberPluginSettings;
 
-	async onload() {
-    this.registerExtensions(['gts', 'gjs'], "javascript");
+  async onload() {
+    this.registerExtensions(["gts", "gjs"], "javascript");
 
     await this.loadSettings();
     this.addSettingTab(new EmberPluginSettingTab(this.app, this));
@@ -25,25 +30,27 @@ export default class EmberPlugin extends Plugin {
   async registerEmberProcessor() {
     let url = new URL(this.settings.renderUrl);
 
-    ["gjs","gts"].forEach(lang => {
-      this.registerMarkdownCodeBlockProcessor(lang, async (source, el, _ctx) => {
-        const highlightElement = async (el: HTMLElement) => {
-          const prism = await loadPrism();
-          prism.highlightElement(el);
-        };
-        let pre = el.createEl('pre');
-        let code = pre.createEl('code', {cls: 'language-js', text: source});
-        highlightElement(code)
+    ["gjs", "gts"].forEach((lang) => {
+      this.registerMarkdownCodeBlockProcessor(
+        lang,
+        async (source, el, _ctx) => {
+          const highlightElement = async (el: HTMLElement) => {
+            const prism = await loadPrism();
+            prism.highlightElement(el);
+          };
+          let pre = el.createEl("pre");
+          let code = pre.createEl("code", { cls: "language-js", text: source });
+          highlightElement(code);
 
-        url.searchParams.set('source', source);
-        url.searchParams.set('lang', lang);
-        const iframe = el.createEl('iframe');
-        iframe.src = url.toString();
-        iframe.width = '100%';
-        el.appendChild(iframe);
-      });
-    })
-
+          url.searchParams.set("source", source);
+          url.searchParams.set("lang", lang);
+          const iframe = el.createEl("iframe");
+          iframe.src = url.toString();
+          iframe.width = "100%";
+          el.appendChild(iframe);
+        },
+      );
+    });
   }
 
   async loadSettings() {
